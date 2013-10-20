33036623413a5412a87bd4d9c143ae9c981d4a4d
Etan Zapinsky <etan.zapinsky@gmail.com>
2013-10-19 23:01:56 -0400

    outlined status api

--- mullitover/urls.py
+++ mullitover/urls.py
@@ -6,6 +6,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', view='web.views.home', name='home'),
     url(r'^stream/$', view='web.views.stream', name='stream'),
-    url(r'^status/$', view='web.views.status', name='status'),
+    url(r'^status/(?P<idfr>\d*)$', view='web.views.status', name='status'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- mullitover/web/views.py
+++ mullitover/web/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.template import Context, loader
 from datetime import datetime
 from django.http import HttpResponse
@@ -12,5 +14,26 @@ def stream(request):
     t = loader.get_template('stream.djhtml')
     return HttpResponse(t.render(Context()))
 
-def status(request):
+def status(request, idfr):
+    if request.method == 'GET':
+        return get_status(request, idfr)
+    elif request.method == 'POST':
+        return post_status(request)
+    elif request.method == 'PUT':
+        return put_status(request, idfr)
+    elif request.method == 'DELETE':
+        return delete_status(request, idfr)
+    return None
+
+
+def get_status(request, idfr):
+    return HttpResponse(json.dumps({"id": idfr}))
+
+def post_status(request):
+    return None
+
+def put_status(request, idfr):
+    return None
+
+def delete_status(request, idfr):
     return None
