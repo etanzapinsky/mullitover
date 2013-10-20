597098279470e3d929f3596afa14f736a802602a
Etan Zapinsky <etan.zapinsky@gmail.com>
2013-10-20 13:07:40 -0400

    almost there

--- mullitover/urls.py
+++ mullitover/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
-admin.autodiscover()
+# from django.contrib import admin
+# admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', view='web.views.home', name='home'),
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
     url(r'^status/(?P<idfr>\d*)$', view='web.views.status', name='status'),
     url(r'^statuses/$', view='web.views.statuses', name='statuses'),
     url(r'^fbauth/$', view='web.views.post_fbauth', name='post_fbauth'),
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 )
